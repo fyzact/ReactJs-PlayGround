93af6691b5c766bfd03641819e953fe2b00e9a2a
fyzact <feyyazacet@gmail.com>
2021-02-19 00:02:37 +0000

    add sideeffect hook to timeout

--- stargame/src/components/StartMatch.js
+++ stargame/src/components/StartMatch.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../assets/App.css';
 import mathUtils   from "../utils/math-utils";
 
@@ -6,16 +6,31 @@ const StarMatch = () => {
   const [stars, setStars]=useState(mathUtils.random(1,9));
   const [availableNumbers,setAvailableNumbers]=useState(mathUtils.range(1,9))
   const [candidateNumbers,setCandidateNumbers]=useState([])
+  const[secondLeft,setSecondLeft]=useState(10);
 
 
   
   const candidateAreWrong=mathUtils.sum(candidateNumbers)>stars;
-  const gameIsDone=availableNumbers.length===0;
+  // const gameIsDone=availableNumbers.length===0;
+  // const timeIsDone=secondLeft===0;
+  const gameStatus= availableNumbers.length===0?"won":secondLeft===0?"lost":"active";
+  useEffect(()=>{
+  
+    if(secondLeft!==0){
+      console.log("rendered");
+       const timeId= setTimeout(()=>setSecondLeft(secondLeft-1),1000)
+       
+       return ()=> clearTimeout(timeId);
+    }
+
+  });
+
 
   const resetGame=()=>{
     setStars(mathUtils.random(1,9));
     setAvailableNumbers(mathUtils.range(1,9));
     setCandidateNumbers([]);
+    setSecondLeft(10);
   }
   
 
@@ -60,7 +75,7 @@ const StarMatch = () => {
       <div className="body">
         <div className="left">
           {
-      gameIsDone?<PlayAgain resetGame={resetGame}/>:  <StartPlay count={stars} />
+          gameStatus!=="active"?<PlayAgain status={gameStatus} resetGame={resetGame}/>:  <StartPlay count={stars} />
           }
      
         </div>
@@ -76,7 +91,7 @@ const StarMatch = () => {
         )}
         </div>
       </div>
-      <div className="timer">Time Remaining: 10</div>
+      <div className="timer">Time Remaining: {secondLeft}</div>
     </div>
   );
 };
@@ -86,6 +101,12 @@ const PlayAgain=(props)=>{
 
   return (
     <div className="game-done">
+      <div 
+      className="message"
+      style={{color:props.status==="lost"?"red":"green"}}
+      >
+        {props.status==="lost"?"Game Over":"Nice"}
+      </div>
     <button  style={{height:"100%",width:"100%"}} onClick={()=> props.resetGame()}>Play Again </button>
     </div>
   )
