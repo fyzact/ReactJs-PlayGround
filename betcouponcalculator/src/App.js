315dbd65b60354d0c04fefe6c546470741ffc6c9
fyzact <feyyazacet@gmail.com>
2021-02-24 00:01:11 +0000

    add selectRate to chose rate

--- betcouponcalculator/src/App.js
+++ betcouponcalculator/src/App.js
@@ -1,11 +1,11 @@
 import './assets/App.css';
-import { useState } from "react";
+import React, { useState } from "react";
 
 
 const matches=[{
   id:1,
-  home:"Fenerbahce",
-  guest:"Galatasaray",
+  host:"Fenerbahce",
+  opponent :"Galatasaray",
   rates:[
    {option:"1", rate:2.00,selected:false},
    {option:"0", rate:2.60,selected:false},
@@ -13,8 +13,8 @@ const matches=[{
  ]
 },{
  id:2,
- home:"Trabzonspor",
- guest:"Beşiktaş",
+ host:"Trabzonspor",
+ opponent:"Beşiktaş",
  rates:[
    {option:"1", rate:2.30,selected:false},
    {option:"0", rate:2.40,selected:false},
@@ -25,13 +25,24 @@ function App() {
  const [selectedList,setSelectedList]=useState([]);
  const [matchList,setmatchList]=useState(matches);
  const selectRate=(id,selected)=>{
-   const match=matches.find(p=>p.id===id);
-   const index=matches.findIndex(p=>p.id===id);
-   const rateIndex= matches[index].rates.findIndex(p=>p.option===selected.option);
-   matches[index].rates[rateIndex].selected=true;
-   console.log(match);
-  console.log(`${id} - select ${selected.option} - rate${selected.rate}`)
-setmatchList(matches);
+
+   const match=matchList.find(p=>p.id===id);
+   const index=matchList.findIndex(p=>p.id===id);
+   const oldRateIndex=match.rates.findIndex(p=>p.selected===true);
+   const newRateIndex=match.rates.findIndex(p=>p.option===selected.option);
+   if(oldRateIndex!==(-1)){
+    matchList[index].rates[oldRateIndex].selected=false;
+   }
+
+   if(oldRateIndex!==newRateIndex)
+   matchList[index].rates[newRateIndex].selected=true;
+
+  const tempSelectedList=selectedList.filter(p=>p.id!==id);
+  const newSelectedList=[...tempSelectedList,{id:id, host:match.host, opponent :match.opponent, predication:selected.option, rate:selected.rate }];
+  setSelectedList(newSelectedList);
+  setmatchList(matchList);
+  console.log(matchList);
+
  }; 
 
 
@@ -56,15 +67,15 @@ const Match=(props)=>{
 return (
   <div className="maclar">
         <div className="evsahibi">
-          <span className="takimName">{props.match.home}</span>
-          <span onClick={()=>props.selected(props.match.id,host)} style={{background: host.selected===true?"purple":"black"}} className="oran">{props.match.rates[0].rate.toFixed(2)}</span>
+          <span className="takimName">{props.match.host}</span>
+          <span onClick={()=>props.selected(props.match.id,host)} style={{background: host.selected===true?"purple":"#b8a9a9"}} className="oran">{props.match.rates[0].rate.toFixed(2)}</span>
         </div>
         <div className="beraberlik">
         <span onClick={()=>props.selected(props.match.id,draw)} style={{background: draw.selected?"purple":"#b8a9a9"}}  className="oran">{props.match.rates[1].rate.toFixed(2)}</span>
         </div>
         <div className="misafir">
         <span onClick={()=>props.selected(props.match.id,guest)}  style={{background: guest.selected?"purple":"#b8a9a9"}} className="oran">{props.match.rates[2].rate.toFixed(2)}</span>
-          <span className="takimName">{props.match.guest}</span>
+          <span className="takimName">{props.match.opponent}</span>
         </div>
       </div>
 );
