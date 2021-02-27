7eefcc7ede91bb0a5875b4b413ad102947573a82
fyzact <feyyazacet@gmail.com>
2021-02-27 23:19:44 +0000

    congirue webpack eslint and babel config

--- react-redux-sample/src/index.js
+++ react-redux-sample/src/index.js
@@ -1,10 +1,40 @@
 import React from  "react";
 import {render} from "react-dom"
 
+//creareclass component
+// var SayHi1=React.createClass({
+//     render:function(){
+//         return (
+//             <div>feyyaz</div>
+//         );
+//     }
+// })
 
+//
 
-const SayHi=(props)=>{
-    return <p>Hi {props.name}</p>
+// class SayHi1 extends React.Component{
+
+//     constructor(props) {
+//         super(props);
+//     }
+    
+//     render(){
+//         return (
+//             <p>Hi2 {this.props.name}</p>
+//         )
+//     }
+    
+// }
+
+//function component
+function SayHi(props){
+    return <p>Hi {props.name}
+    </p>
 }
+
+// const SayHi=(props)=>{
+//     return <p>Hi {props.name}
+//     </p>
+// }
 
 render(<SayHi  name="Feyyaz"/>,document.getElementById("app"))
