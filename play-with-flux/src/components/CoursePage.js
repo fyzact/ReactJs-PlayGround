07c16642f73f947005060810932e635e7aecb87e
fyzact <feyyazacet@gmail.com>
2021-09-12 16:35:59 +0300

    change coursespage from class component to function component

--- play-with-flux/src/components/CoursePage.js
+++ play-with-flux/src/components/CoursePage.js
@@ -1,48 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component{
+function CoursesPage (){
+    const [courses,setCourses]=useState([]);
+    
+    useEffect(()=>{
+        getCourses().then(_courses=>setCourses(_courses));
+    },[])
 
-    state={
-        courses:[]
-    };
-
-    componentDidMount(){
-           getCourses().then(courses=>this.setState({courses:courses}));
-    }
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         courses:[]
-    //     }
-
-    // }
-
-    renderRow(course){
+     function   renderRow(course){
         return <tr key={course.id}>
         <td>{course.title}</td>
         <td>{course.authorId}</td>
         <td>{course.category}</td>
     </tr>
     }
-    render(){
-     
-    return     <>
-<h2>Courses</h2>
+
+    return <>
+    <h2>Courses</h2>
       <table className="table">
-            <thead>
+             <thead>
                 <tr>
-                    <th>Title</th>
+                   <th>Title</th>
                     <th>Autohr ID</th>
-                    <th>Category</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.state.courses.map(this.renderRow)}
-            </tbody>
-        </table>
-        </>
-    }
+                   <th>Category</th>
+               </tr>
+          </thead>
+           <tbody>
+               {courses.map(renderRow)}
+           </tbody>
+       </table>
+    </>
+
+
 }
+
+// class CoursesPage extends React.Component{
+
+//     state={
+//         courses:[]
+//     };
+
+//     componentDidMount(){
+//            getCourses().then(courses=>this.setState({courses:courses}));
+//     }
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state={
+//     //         courses:[]
+//     //     }
+
+//     // }
+
+//     renderRow(course){
+//         return <tr key={course.id}>
+//         <td>{course.title}</td>
+//         <td>{course.authorId}</td>
+//         <td>{course.category}</td>
+//     </tr>
+//     }
+//     render(){
+     
+//     return     <>
+// <h2>Courses</h2>
+//       <table className="table">
+//             <thead>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Autohr ID</th>
+//                     <th>Category</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                 {this.state.courses.map(this.renderRow)}
+//             </tbody>
+//         </table>
+//         </>
+//     }
+// }
 
 export default CoursesPage;
