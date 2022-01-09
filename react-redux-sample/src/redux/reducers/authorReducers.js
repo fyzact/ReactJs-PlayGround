e1e10efbabc649d151298a6056cfc61731f0e943
fyzact <feyyazacet@gmail.com>
2022-01-09 00:46:28 +0000

    added initial state both authors and courses

--- react-redux-sample/src/redux/reducers/authorReducers.js
+++ react-redux-sample/src/redux/reducers/authorReducers.js
@@ -1,6 +1,7 @@
 import * as actionTypes from "../actions/actionTypes"
+import initialState from '../reducers/initialState';
 
-export default function authorReducers(state=[], action)
+export default function authorReducers(state=initialState.authors, action)
 {
     switch (action.type) {
         case actionTypes.LOAD_AUTHORS:
