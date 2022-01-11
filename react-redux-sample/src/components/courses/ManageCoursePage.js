ed7c3e537eae4466db82e365e5a7cf8a05cfd050
fyzact <feyyazacet@gmail.com>
2022-01-11 23:26:16 +0000

    mange course page converted from class to functional component

--- react-redux-sample/src/components/courses/ManageCoursePage.js
+++ react-redux-sample/src/components/courses/ManageCoursePage.js
@@ -1,49 +1,57 @@
-import React from "react";
-import {connect} from "react-redux"
-import {loadCourses} from "../../redux/actions/courseActions"
-import {loadAuthors} from "../../redux/actions/authorActions"
-import PropTypes from "prop-types"; 
+import React, {
+    useEffect
+} from "react";
+import {
+    connect
+} from "react-redux"
+import {
+    loadCourses
+} from "../../redux/actions/courseActions"
+import {
+    loadAuthors
+} from "../../redux/actions/authorActions"
+import PropTypes from "prop-types";
 
 
-class ManageCoursesPage extends React.Component{
+function ManageCoursesPage({
+    courses,
+    authors,
+    loadCourses,
+    loadAuthors
+}) {
 
-    componentDidMount(){
-    const {courses, authors, loadCourses, loadAuthors}=this.props;
-
-        if(courses.length===0 ){
+    useEffect(() => {
+        if (courses.length === 0) {
             loadCourses();
         }
 
-        if(authors.length===0){
+        if (authors.length === 0) {
             loadAuthors();
         }
-       
-    }
-  render (){
-      return (
-          <>
-          <h2>Manage Course</h2>
-         
-          </>
-        )
-    }
-       
+    },[])
+
+    return ( 
+        <>
+        <h2 > Manage Course </h2>
+        </>
+    )
 }
-function mapStateToProps(state){
+
+function mapStateToProps(state) {
     return {
         courses: state.courses,
-        authors:state.authors
+        authors: state.authors
     }
 }
 
-const mapDispatchToProps={
-        loadCourses,
-        loadAuthors
+const mapDispatchToProps = {
+    loadCourses,
+    loadAuthors
 }
-ManageCoursesPage.propTypes={
-    courses:PropTypes.array.isRequired,
-    authors:PropTypes.array.isRequired,
-    loadCourses:PropTypes.func.isRequired,
-    loadAuthors:PropTypes.func.isRequired
+ManageCoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired
 }
-export default connect(mapStateToProps,mapDispatchToProps)(ManageCoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
